d7c1721d5d77fd5b395ff358bfeb113b164acfc5
unknown <priyankachauhan031999@gamil.com>
2024-11-04 23:00:14 +0530

    my code

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "top-rg7" {
+  name     = "limited"
+  location = "west us"
+}
+
+resource "azurerm_resource_group" "rg-pc" {
+  name     = "rg-pc"
+  location = "west us"
+}
+
+resource "azurerm_resource_group" "rg-priyanka" {
+  name     = "rg-priyanka"
+  location = "centralindiagit"
+}
